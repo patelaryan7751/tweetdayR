7d9e9eda59035db68d3f245183183503b1bc4431
patelaryan7751 <59426397+patelaryan7751@users.noreply.github.com>
2021-03-07 16:43:30 +0530

    added validation

--- frontend/work.js
+++ frontend/work.js
@@ -4,14 +4,34 @@ var tdd = String(today.getDate() + 1).padStart(2, "0");
 var ydd = String(today.getDate() - 1).padStart(2, "0");
 var mm = String(today.getMonth() + 1).padStart(2, "0");
 var yyyy = today.getFullYear();
-today = ydd + "-" + mm + "-" + yyyy;
-document.getElementById("dyn").textContent = `Do not choose dates after ${today}`;
+yesterday = ydd + "-" + mm + "-" + yyyy;
+ysef= yyyy+ "-" + mm + "-" + ydd;
+document.getElementById("dyn").textContent = `Do not choose dates after ${yesterday} .  ${yesterday} this date can be choosen`;
 document.querySelector("#sbtn").onclick = function () {
     var dti = document.getElementById("dt").value.split("-").join("");
     console.log(dti);
     url = `https://patelaryan7751.github.io/tweetdayR/frontend/tweet.html?id=${dti}`;
     console.log(url);
-    document.getElementById("tbtn").style.display = "block";
+    if(dti){
+        var x= new Date('2021-03-05');
+        var y=new Date(`${ysef}`);
+        var z=new Date(`${document.getElementById("dt").value}`);
+       if(x<=z && z<=y){
+           console.log("true")
+           document.getElementById("val").style.display = "none";
+            document.getElementById("tbtn").style.display = "block";
+       }
+        else{
+            document.getElementById("val").style.display = "block";
+             document.getElementById("tbtn").style.display = "none";
+            console.log("false")
+        }
+   
+    }
+    else{
+         document.getElementById("val").style.display = "block";
+        document.getElementById("tbtn").style.display = "none";
+    }
 };
 document.querySelector("#tbtn").onclick = function () {
     window.open(`${url}`, "_self");
